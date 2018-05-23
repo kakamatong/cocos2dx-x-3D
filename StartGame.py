3f107383b79f4b076ce5c588ba87dab1b9167888
kakamatong <631603920@qq.com>
2018-05-23 23:53:13 +0800

    使用python脚本启动exe客户端，可以多开

---
+++ StartGame.py
@@ -0,0 +1,12 @@
+import os
+
+exePath = "simulator/win32/cocos2dx_3D.exe"
+workDir = os.getcwd()
+mainFile = "src/main.lua"
+
+fullPath = os.path.join(workDir, exePath)
+print(fullPath)
+
+command = "%s -workdir %s -file %s" % (fullPath, workDir, mainFile)
+
+os.system(command)
