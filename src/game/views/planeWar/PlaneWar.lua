d34245b2957df29a01d170cc2e36ac1054f8b9cb
kakamatong <631603920@qq.com>
2019-01-06 15:44:43 +0800

    bullet

--- src/game/views/planeWar/PlaneWar.lua
+++ src/game/views/planeWar/PlaneWar.lua
@@ -5,12 +5,20 @@ PlaneWar.KK_TOUCH_PNL = "KK_TOUCH_PNL"
 PlaneWar.KK_PLANE = "KK_PLANE"
 PlaneWar.touchb = 0
 PlaneWar.planeWidth = 102
+PlaneWar.planeHeight = 126
+PlaneWar.launchTime = 0.15
+PlaneWar.bulletInfo = {
+    time = 2,
+    range = 1000
+}
+PlaneWar.bullets = {}
 function PlaneWar:onCreate()
     print("onCreate")
 
     UIUtils.addTouchEventListener(self.resourceNode_, self.KK_BTN_CLOSE, handler(self, self.onTouchEvent))
     UIUtils.addTouchEventListener(self.resourceNode_, self.KK_TOUCH_PNL, handler(self, self.onTouchPnl))
     self.plane = UIUtils.findNodeByName(self.resourceNode_, self.KK_PLANE)
+    self:startBullet()
 end
 
 function PlaneWar:onTouchEvent(ref, eventType)
@@ -78,7 +86,41 @@ function PlaneWar:planeMove(dx)
     if self:checkBoundary(nowx) then
         self.plane:setPositionX(nowx)
     end
-    
+end
+
+function PlaneWar:startBullet()
+    local sche = cc.Director:getInstance():getScheduler()
+    self.scheID = sche:scheduleScriptFunc(handler(self, self.createBulletAndMove), self.launchTime, false)
+end
+
+function PlaneWar:createBulletAndMove()
+    local bullet = ccui.ImageView:create("Game/PlaneWar/imge/bullet1.png",ccui.TextureResType.plistType)
+    local x,y = self.plane:getPosition()
+    bullet:setPosition(cc.p(x, y + self.planeHeight / 2))
+    self:addBullet(bullet)
+    local moveBy = cc.MoveBy:create(self.bulletInfo.time, cc.p(0,self.bulletInfo.range))
+    local callBack = cc.CallFunc:create(
+        function()
+            self:removeBullet(bullet)
+        end
+    )
+    local seq = cc.Sequence:create(moveBy, callBack)
+    bullet:runAction(seq)
+end
+
+function PlaneWar:addBullet(node)
+    table.insert(self.bullets, node)
+    self.resourceNode_:addChild(node)
+end
+
+function PlaneWar:removeBullet(node)
+    for k, v in pairs(self.bullets) do
+        if v == node then
+            self.bullets[k] = nil
+            v:removeFromParent()
+        end
+    end
+
 end
 
 function PlaneWar:onBtnClose()
@@ -88,6 +130,13 @@ function PlaneWar:onBtnClose()
         defaultSceneName = "LobbyScene",
     }
     require("lobby.LobbyApp"):create(configs):run()
+end
+
+function PlaneWar:onExit()
+    if self.scheID then
+        local sche = cc.Director:getInstance():getScheduler()
+        sche:unscheduleScriptEntry(self.scheID)
+    end
 end
 
 return PlaneWar
